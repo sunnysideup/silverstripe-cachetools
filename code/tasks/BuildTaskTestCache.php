801fc699a8df08e62d6857fe04b19646af66d1ce
Nicolaas <github@sunnysideup.co.nz>
2016-12-07 06:36:04 +1300

    Update BuildTaskTestCache.php

--- code/tasks/BuildTaskTestCache.php
+++ code/tasks/BuildTaskTestCache.php
@@ -1,16 +1,17 @@
 <?php
 
-class EcommerceTaskCacheTest extends BuildTask
+class BuildTaskTestCache extends BuildTask
 {
     protected $title = 'Test Silverstripe Cache';
 
-    protected $description = 'Basic test for the silverstripe cache. It will show the date and time the cache was made.';
+    protected $description = '
+        Basic test for the Sillverstripe Cache. 
+        It will show the date and time the cache was made.';
 
     public function run($request)
     {
-        $cachekey = 'foo';
-        $cache = SS_Cache::factory($cachekey);
-        $result = $cache->load($cachekey);
+        $cache = SS_Cache::factory('foo');
+        $result = $cache->load('bar');
         if (!$result || isset($_GET['reload'])) {
             $time = time();
             for($i = 1; $i < $time; $i = $i + 75) {
@@ -18,7 +19,7 @@ class EcommerceTaskCacheTest extends BuildTask
             }
             $result = date('Y-m-d H:i:s');;
             DB::alteration_message('not from cache: '.$result, 'deleted');
-            $cache->save($result, $cachekey);
+            $cache->save($result, 'bar');
         } else {
             DB::alteration_message('from cache: '.$result, 'created');
         }
